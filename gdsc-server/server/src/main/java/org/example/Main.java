6df1d06a84f5d1a84230bc930b097f5f53044be8
hwangjeyeon <kingpele9018@gmail.com>
2024-02-25 22:06:17 +0900

    11강 최초 커밋

---
+++ gdsc-server/server/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ gdsc-server/server/src/main/java/org/example/controller/CrudController.java
@@ -0,0 +1,54 @@
+package org.example.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/crud-api")
+public class CrudController {
+
+    @GetMapping
+    public String getName(){
+        return "Flature";
+    }
+
+    @GetMapping(value="/{variable}")
+    public String getVariable(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping("/param")
+    public String getNameWithParam(String name){
+        return "Hello. " + name + "!";
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberDto> getMember(
+            @RequestBody MemberDto request,
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ){
+        System.out.println(request.getName());
+        System.out.println(request.getEmail());
+        System.out.println(request.getOrganization());
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName(name);
+        memberDto.setEmail(email);
+        memberDto.setOrganization(organization);
+
+        return ResponseEntity.status(HttpStatus.OK).body(memeberDto);
+    }
+
+    @PostMapping(value = "/add-header")
+    public ResponseEntity<MemberDto> addHeader(@RequestHeader("my-header") String header,
+                                               @RequestBody MemberDto memberDto){
+        System.out.println(header);
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberDto);
+    }
+
+}
